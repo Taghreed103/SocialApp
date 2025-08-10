45c20d7d5999bf26d064061ed672ea7a728c91b7
YARA <yara.aldoasri@gmail.com>
2025-08-10 05:25:02 +0300

    update  color of text area label with comment

--- src/Components/CreateComment.jsx
+++ src/Components/CreateComment.jsx
@@ -32,10 +32,10 @@ export default function CreateComment({id}) {
 
     
   <div  className='p-5'>
-  <label htmlFor="message" className="block mb-2 text-sm font-medium text-left text-gray-900 dark:text-white">Your Comment here .......</label>
+  <label htmlFor="message" className="block mb-2 text-sm font-medium text-left text-purple-800 dark:text-purple-600">Your Comment here .......</label>
   <textarea  
 
-   onKeyDown={addComment} id="message" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..." defaultValue={""} />
+   onKeyDown={addComment} id="message" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black-600 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..." defaultValue={""} />
 </div>
 
     
